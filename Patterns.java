7244d93d38536b47fc0c18e063493654cd8f132b
umme019 <afsanaara88@gmail.com>
2026-08-11 17:24:59 +0530

    Add a hollow butterfly

--- Patterns.java
+++ Patterns.java
@@ -8,29 +8,58 @@ public class Patterns {
           //upper half
             for(int i = 1; i<=n; i++){
                    //1st half
-                   for (int j = 1; j<=i; j++){
+                   
+             if (i <=2){
+                 for (int j =1;j<=i ;j++){
                     System.out.print("*");
+                }
+            }else{
+                    System.out.print("*");
+                    for (int j =1; j<=i-2;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
                    }
+                
+            
 
                    //spaces
                    int spaces = 2 * (n-i);
                    for (int j =1;j<=spaces ; j++){
                     System.out.print(" ");
                    }
+                
 
                    //2nd half
-                   for (int j=1 ;j<=i;j++){
+                      if (i <=2){
+                 for (int j =1;j<=i ;j++){
                     System.out.print("*");
+                }
+            }else{
+                    System.out.print("*");
+                    for (int j =1; j<=i-2;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
                    }
                    System.out.println();
-    }
+         }
+        
 
          //lower half
          for(int i = n; i>=1; i--){
                    //1st half
-                   for (int j = 1; j<=i; j++){
+                      if (i <=2){
+                 for (int j =1;j<=i ;j++){
                     System.out.print("*");
-                   }
+                }
+            }else{
+                    System.out.print("*");
+                    for (int j =1; j<=i-2;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
 
                    //spaces
                    int spaces = 2 * (n-i);
@@ -38,16 +67,27 @@ public class Patterns {
                     System.out.print(" ");
                    }
 
+
                    //2nd half
-                   for (int j=1 ;j<=i;j++){
+                   if (i <=2){
+                 for (int j =1;j<=i ;j++){
                     System.out.print("*");
-                   }
-                   System.out.println();
-    }
+                }
+            }else{
+                    System.out.print("*");
+                    for (int j =1; j<=i-2;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+                   } 
+                System.out.println();
 
 }
 }
 }
+}
+
+
 
 
 
