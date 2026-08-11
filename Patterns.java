72b12f6d0eacce2d295546038010903a5193a751
umme019 <afsanaara88@gmail.com>
2026-08-11 15:50:20 +0530

    Add a File

--- Patterns.java
+++ Patterns.java
@@ -4,22 +4,52 @@ public class Patterns {
     public static void main(String[]args) {
          Scanner sc = new Scanner(System.in); {
             int n = 4;
-            int m = 5;
 
-            for (int i = 1;i<=n; i++){
-                
-            for (int j=1 ; j<=m ; j++){ 
-                System.out.print("*");
-            
-            }
+          //upper half
+            for(int i = 1; i<=n; i++){
+                   //1st half
+                   for (int j = 1; j<=i; j++){
+                    System.out.print("*");
+                   }
 
-            
-             System.out.println();
-            }
-            
-}
-}
+                   //spaces
+                   int spaces = 2 * (n-i);
+                   for (int j =1;j<=spaces ; j++){
+                    System.out.print(" ");
+                   }
+
+                   //2nd half
+                   for (int j=1 ;j<=i;j++){
+                    System.out.print("*");
+                   }
+                   System.out.println();
     }
+
+         //lower half
+         for(int i = n; i>=1; i--){
+                   //1st half
+                   for (int j = 1; j<=i; j++){
+                    System.out.print("*");
+                   }
+
+                   //spaces
+                   int spaces = 2 * (n-i);
+                   for (int j =1;j<=spaces ; j++){
+                    System.out.print(" ");
+                   }
+
+                   //2nd half
+                   for (int j=1 ;j<=i;j++){
+                    System.out.print("*");
+                   }
+                   System.out.println();
+    }
+
+}
+}
+}
+
+
 
 
 
