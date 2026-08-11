be1266cbc9b318dba3063339a134cc2e710e714f
umme019 <afsanaara88@gmail.com>
2026-08-11 19:07:32 +0530

    Add a inverted half triangle

--- Patterns.java
+++ Patterns.java
@@ -6,15 +6,12 @@ public class Patterns {
             int n = 5;
 
             for (int i=1; i<=n; i++){
-
-                for (int j=1;j<=n-i; j++){
-                    System.out.print(" ");
-                }
-                for (int j=1; j<=i ;j++){
-                    System.out.print(j + " ");
+             for (int j =1;j<= 5 -i +1;j++){
+                    System.out.print(i);
                 }
                 System.out.println();
-            }
-        }
+                
+    }
     } 
+}
 }
