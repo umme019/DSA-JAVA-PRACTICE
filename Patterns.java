8d61fd6942eabb418789a59d4045bacd0a63deec
umme019 <afsanaara88@gmail.com>
2026-08-11 19:52:50 +0530

    Add a Pascal Triangle

--- Patterns.java
+++ Patterns.java
@@ -4,13 +4,25 @@ public class Patterns {
     public static void main(String[]args) {
          Scanner sc = new Scanner(System.in); {
             int n = 5;
+            
 
-            for (int i=1; i<=n; i++){
-             for (int j =1;j<= 5 -i +1;j++){
-                    System.out.print(i);
+            for (int i=0; i<=n; i++){
+
+                for (int j=1; j<=n-i ; j++){
+                    System.out.print(" ");
+                }
+                if ( i == 0){
+                    System.out.print("1");
+                }else {
+                int ncr =1;
+                System.out.print(ncr + " ");
+                for (int j=1 ;j<=i ;j++){
+                      ncr = ncr * (i -j +1)/j ;
+                      System.out.print(ncr + " ");
+                }
+                    
                 }
                 System.out.println();
-                
     }
     } 
 }
