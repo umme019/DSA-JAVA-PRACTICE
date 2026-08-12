1d499c3fa4b528fde40742714d874f9e0a5d6177
umme019 <afsanaara88@gmail.com>
2026-08-12 16:56:56 +0530

    Add a Table

--- Function.java
+++ Function.java
@@ -1,22 +1,19 @@
 import java.util.*;
 
 public class Function {
-    public static void checkNumber (int n){
+    public static void printTable(int n){
 
-
-            if (n % 2 == 0){
-                System.out.println("Even Number");
-            }else {
-                System.out.println("Odd Number");
-            }
-            return;
+           for (int j =1;j<=10; j++){
+            System.out.println(n + " * " + j + " = " + (n*j));
+           }
+          return;
         }
         
     public static void main(String[]arg){
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
 
-    checkNumber(n);
+    printTable(n);
 
 }
 }
