2fc085345551c646757713018dee6253f14f67bd
umme019 <afsanaara88@gmail.com>
2026-08-12 16:47:29 +0530

    Add a File

--- Function.java
+++ Function.java
@@ -4,20 +4,10 @@ public class Function {
     public static void checkNumber (int n){
 
 
-            if (n == 1){
-                System.out.println("Not prime nor composite");
-            }else if (n == 0){
-            System.out.println("Not a prime number");
-    
+            if (n % 2 == 0){
+                System.out.println("Even Number");
             }else {
-                for (int i= 2; i<n ;i++){
-                   if (n % i == 0){
-                     System.out.println("Not a prime number");
-                     return;
-                
-                   }
-                }
-                System.out.println("Prime number");
+                System.out.println("Odd Number");
             }
             return;
         }
