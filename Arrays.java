03d2e62b6f102723b954167e7bada025ca2fe4ed
umme019 <afsanaara88@gmail.com>
2026-08-15 00:08:09 +0530

    Add a max and min of integers

--- Arrays.java
+++ Arrays.java
@@ -4,17 +4,30 @@ public class Arrays{
     public static void main (String args[]){
         Scanner sc = new Scanner (System.in);
 
-        int size = sc.nextInt();
-        String name []= new String [size];
+       int size = sc.nextInt();
+        int number [] = new int [size];
 
         //input
-        for(int i=0; i<size;i ++){
-            name[i]= sc.next();
+        for (int i= 0; i<size ; i++){
+            number[i] = sc.nextInt();
         }
 
+        int max= Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
         //output
-        for(int i=0; i<size;i++){
-            System.out.println(name[i]);
+        for (int i = 0; i<size; i++){
+            if (number[i] > max){
+                  max = number[i];
+            }
+
+            if (number[i]< min){
+                min = number[i];
+            }
+
         }
+        System.out.println("Max :" + max);
+        System.out.println("Min :" + min);
+
     }
 }
