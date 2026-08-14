b233785f21290c638e4a7f81a3e881c2e2701139
umme019 <afsanaara88@gmail.com>
2026-08-14 13:51:27 +0530

    Add a file

--- Function.java
+++ Function.java
@@ -1,17 +1,25 @@
 import java.util.*;
 
 public class Function {
+    public static int printValue(int x,int n){
+
+         int result = 1;
+         for(int i =1; i<=n;i ++){
+            result *= x;
+         }
+         return result;
+    }
     
     
 
         
     public static void main(String[]arg){
     Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
     int n = sc.nextInt();
 
-     int Circumference = printCircumference(n);
+    System.out.println(printValue(x, n));
 
-    System.out.println(Circumference);
     
 
     
