7b6ef59b6d39e9ddc7cdef7b1e6ca6fb0443dc3a
umme019 <afsanaara88@gmail.com>
2026-08-14 13:36:36 +0530

    Add a file

--- Loop.java
+++ Loop.java
@@ -2,20 +2,42 @@ import java.util.*;
  
 public class Loop {
     public static void main(String[]args){
-      Scanner sc = new Scanner (System.in);{
-          
-        int i = 1;
+     Scanner sc = new Scanner(System.in);
+
+        int pos = 0,neg = 0,zero = 0;
+        String choice;
 
         do{
-          System.out.println(" " + i);
-          i ++;
-        }while (true);
+          System.out.println("Enter a Number");
+          int num = sc.nextInt();
+
+          if (num > 0)pos++;
+          else if (num < 0) neg++;
+          else zero ++;
+
+          System.out.println("Do you want to continue? (yes / no)");
+           choice = sc.next();
+        }while (choice . equals("no"));
+
+        System.out.println("Positive : " + pos);
+        System.out.println("Negative :" + neg);
+        System.out.println("Zeros :" + zero);
       }
     }
 
         
 
-    }
+    
+
+
+
+
+
+
+
+        
+
+    
 
 
 
