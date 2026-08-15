8cdf7168ed6dde7727a722e1c038d9477c66188f
umme019 <afsanaara88@gmail.com>
2026-08-16 00:13:10 +0530

    Add a file to find x

--- Arrays.java
+++ Arrays.java
@@ -18,12 +18,16 @@ public class Arrays{
 
         }
 
+        int x = sc.nextInt();
+
         //output
         for (int i = 0;i< rows;i++){
             for  (int j =0;j<columns ; j++){
-               System.out.print(numbers[i][j] + " ");
+                //compare with x
+                if (numbers [i][j] == x){
+             System.out.println("x found at location (" + i + "," + j + ") ");
+                }
             }
-               System.out.println();
         }
 
      
