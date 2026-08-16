decbe76475608f1c14dab28bd0c52141e99dd9d9
umme019 <afsanaara88@gmail.com>
2026-08-16 19:31:26 +0530

    Add a Spiral Matrix

--- Arrays.java
+++ Arrays.java
@@ -18,16 +18,49 @@ public class Arrays{
    }
 
         //output
-        for (int j = 0;j< m;j++){
-            for  (int i =0;i<n ; i++){
-                System.out.print(matrix[i][j] + " ");
-                }
-                System.out.println();
+        int rowStart = 0;
+        int rowEnd = n - 1;
+        int colStart = 0;
+        int colEnd = m - 1;
+
+        while (rowStart <= rowEnd && colStart <= colEnd){
+
+            //1.First Row (left to right)
+            for (int col=colStart;col<=colEnd;col++){
+                System.out.print(matrix[rowStart][col] + " ");
+                
             }
+            rowStart++;
+
+            //2.Last column (top to bottom)
+            for(int row=rowStart; row<=rowEnd;row++){
+                System.out.print(matrix[row][colEnd] + " ");
+               
+            }
+             colEnd--;
+
+            //3.Last row (right to left)
+            if (rowStart <=rowEnd){
+            for (int col=colEnd; col>=colStart;col--){
+            System.out.print(matrix[rowEnd][col] + " ");
+            }
+              rowEnd--;
+            }
+
+            //4.First column (bottom to top)
+            if (colStart<= colEnd){
+            for(int row = rowEnd;row>=rowStart;row--){
+                  System.out.print(matrix[row][colStart] + " ");
+            }
+                    colStart++;
+            }
+           
         }
+         System.out.println();
 
      
     }
+}
 
         
 
