b95f55ad0ce14caf6873c32368851a80f80a9505
umme019 <afsanaara88@gmail.com>
2026-08-16 19:54:55 +0530

    Add use of charAt

--- Strings.java
+++ Strings.java
@@ -4,16 +4,19 @@ public class Strings{
     public static void main (String args[]){
         Scanner sc = new Scanner (System.in);
 
-        int size = sc.nextInt();
-        String array [] = new String [size];
-        int totLength = 0;
+        String str = sc.next();
+        String result = " ";
 
-        for (int i = 0;i<size ; i++){
-            array[i]= sc.next();
-            totLength += array[i].length();
+        for (int i = 0; i<str.length(); i++){
+            if (str.charAt(i) == 'e'){
+                result += 'i';
 
+            }else{
+                result += str.charAt(i);
+            }
         }
-        System.out.println(totLength);
+        System.out.println(result);
+
 
     }
 }
