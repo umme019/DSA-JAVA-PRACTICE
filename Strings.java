0d5d41bd0edea6e26292d25a741dab39f5c83886
umme019 <afsanaara88@gmail.com>
2026-08-17 23:24:25 +0530

    Add a palindrome

--- Strings.java
+++ Strings.java
@@ -1,8 +1,10 @@
 import java.util.*;
 
 public class Strings{
-    public static void isPalindrome(String str){
-        
+    public static void main(String []args){
+     Scanner sc = new Scanner (System.in);
+
+        String str = sc.next();
         StringBuilder sb = new StringBuilder(str);
         StringBuilder reversed = new StringBuilder(str);
         reversed.reverse();
