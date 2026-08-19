d127543941b88fac65f8ca3873bca23e1a56d55e
umme019 <afsanaara88@gmail.com>
2026-08-20 00:35:40 +0530

    Add a file to convert one number system to another

--- Bits.java
+++ Bits.java
@@ -5,17 +5,20 @@ public class Bits {
     public static void main(String[]args){
      Scanner sc = new Scanner(System.in);
 
-     int n = sc.nextInt();
-     String binary = Integer.toBinaryString(n);
+     System.out.println("Enter type");
+     String type = sc.next();
+      
+     if (type.equals("decimal")){
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        System.out.println("Decimal to Binary = " +Integer.toBinaryString(n));
 
-        int count = 0;
-     for (int i =0;i<binary.length();i ++){
-     if(binary.charAt(i) == '1'){
-        count++;
-        
+     }else if(type.equals("binary")){
+        System.out.println("Enter number");
+         String binary = sc.next();
+        System.out.println("Binary to Decimal = " + Integer.parseInt(binary,2));
      }
-    }
-    System.out.println(count);
+
 
      
 
