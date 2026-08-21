52c8722eca99139c36e600cf7306f747e68d84e7
umme019 <afsanaara88@gmail.com>
2026-08-21 18:03:42 +0530

    Add a selection sort

--- Sorting.java
+++ Sorting.java
@@ -12,19 +12,21 @@ class Sorting {
     public static void main(String args[]){
    int arr[] = {7,8,3,1,2};
 
-   //bubble sort
+   //selection sort
    for(int i =0; i<arr.length-1; i++){
-    for (int j =0; j<arr.length-i-1; j++){
-        if (arr [j]> arr[j+1]){
-
-            //swap
-            int temp = arr [j];
-            arr [j] = arr [j+1];
-            arr [j+1] = temp;
-
+    int smallest = i;
+    for (int j =i + 1; j<arr.length; j++){
+        if (arr [smallest] > arr [j]){
+        smallest = j;
         }
     }
-   }
+            //swap
+            int temp = arr [smallest];
+            arr [smallest] = arr [i];
+            arr [i] = temp;
+
+        }
+   
     printArray(arr);
 
     }
