44ae7422de38edb245197ba5cb6c80b687c07b96
umme019 <afsanaara88@gmail.com>
2026-08-21 18:20:18 +0530

    Add a insertion sort

--- Sorting.java
+++ Sorting.java
@@ -13,21 +13,19 @@ class Sorting {
    int arr[] = {7,8,3,1,2};
 
    //selection sort
-   for(int i =0; i<arr.length-1; i++){
-    int smallest = i;
-    for (int j =i + 1; j<arr.length; j++){
-        if (arr [smallest] > arr [j]){
-        smallest = j;
-        }
+   for(int i =1; i<arr.length; i++){
+    int current = arr[i];
+    int j= i-1;
+    while (j>=0 && current<arr[j]){
+        arr[j +1]= arr[j];
+        j--;
     }
-            //swap
-            int temp = arr [smallest];
-            arr [smallest] = arr [i];
-            arr [i] = temp;
 
-        }
-   
-    printArray(arr);
+    //placement
+    arr[j + 1]=current;
+}
+     printArray(arr);
+    
 
     }
 }
