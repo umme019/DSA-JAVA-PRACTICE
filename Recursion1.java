fba7cc12af912f58168a89948641bd7f57dc89f8
umme019 <afsanaara88@gmail.com>
2026-08-23 23:55:44 +0530

    Add a Fibonacci Sequence

--- Recursion1.java
+++ Recursion1.java
@@ -1,23 +1,21 @@
 public class Recursion1 {
-    public static int calcfactorial(int n){
-
-        if (n==1 || n==0){
-            return 1;
+    public static void printFib(int a,int b, int n){
+        if (n == 0){
+            return;
         }
-        
-        int fact_nm1 = calcfactorial(n-1);
-        int fact_n = n * fact_nm1;
-          return fact_n;
-            
-        }
-        
+        int c=a+b;
+      System.out.println(c);
+      printFib(b, c, n-1);
+    }
     
     
 
     public static void main(String args[]) {
-     int n = 5;
-     int ans = calcfactorial(n);
-     System.out.println(ans);
+     int a = 0,b=1;
+     System.out.println(a);     
+     System.out.println(b);
+     int n =7;
+     printFib(a,b,n-2);
 
     }    
 }
