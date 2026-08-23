440ffe51163cc43bffee0db539cfc35a89de6179
umme019 <afsanaara88@gmail.com>
2026-08-24 00:26:09 +0530

    Add a Stack Height

--- Recursion1.java
+++ Recursion1.java
@@ -1,21 +1,23 @@
 public class Recursion1 {
-    public static void printFib(int a,int b, int n){
-        if (n == 0){
-            return;
+    public static int calcPower(int x,int n){
+        if (n == 0){ // base case 1
+            return 1;
         }
-        int c=a+b;
-      System.out.println(c);
-      printFib(b, c, n-1);
+        if (x == 0){ //base case 2
+            return 0;
+        }
+        int xPownm1 = calcPower(x, n-1); //work
+      int xPown = x* xPownm1;
+      return xPown;
+      
     }
     
     
 
     public static void main(String args[]) {
-     int a = 0,b=1;
-     System.out.println(a);     
-     System.out.println(b);
-     int n =7;
-     printFib(a,b,n-2);
+     int x = 2, n = 5;
+     int ans = calcPower(x,n);
+     System.out.println(ans);
 
     }    
 }
