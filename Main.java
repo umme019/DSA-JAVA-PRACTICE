4c26b23906b6dae251e2d842963e7fb63ababcf4
umme019 <afsanaara88@gmail.com>
2026-08-25 01:14:42 +0530

    Add a table

--- Main.java
+++ Main.java
@@ -4,14 +4,18 @@ public class Main {
 	
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int radius = sc.nextInt();
+        System.out.println("Enter table number");
+        int num = sc.nextInt();
 
-        double area= Math.PI*radius*radius;
-        double circumference = 2*Math.PI*radius;
-
-        System.out.println("Area =" + Math.round(area));
-        System.out.println("Circumference =" + Math.round(circumference));
-
-
+       System.out.println(num + "*" + "1" + "=" + num*1);
+       System.out.println(num + "*" + "2" + "=" + num*2);
+       System.out.println(num + "*" + "3" + "=" + num*3);
+       System.out.println(num + "*" + "4" + "=" + num*4);
+       System.out.println(num + "*" + "5" + "=" + num*5);
+       System.out.println(num + "*" + "6" + "=" + num*6);
+       System.out.println(num + "*" + "7" + "=" + num*7);
+       System.out.println(num + "*" + "8" + "=" + num*8);
+       System.out.println(num + "*" + "9" + "=" + num*9);
+       System.out.println(num + "*" + "10" + "=" + num*10);
     }
 }
