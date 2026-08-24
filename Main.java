c26c97031a48c02b4f831fad3e3b987a78a91e94
umme019 <afsanaara88@gmail.com>
2026-08-25 01:01:35 +0530

    Add age file

--- Main.java
+++ Main.java
@@ -3,36 +3,9 @@ import java.util.*;
 public class Main {
 	
     public static void main(String[] args) {
-        int i = 4;
-        double d = 4.0;
-        String s = "HackerRank ";
-		
-        Scanner scan = new Scanner(System.in);
-          int n = scan.nextInt();
-          scan.nextLine();
-          double m = scan.nextDouble();
-          scan.nextLine();
-          String Statement = scan.nextLine();
-    System.out.println(i + n);
-    System.out.println(d + m);
-    System.out.println("HackerRank " + Statement  );
-    
-    
-        
-        
+        Scanner sc = new Scanner (System.in);
+        int age = sc.nextInt();
+        System.out.println(age);
 
-        /* Declare second integer, double, and String variables. */
-
-        /* Read and save an integer, double, and String to your variables.*/
-        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
-
-        /* Print the sum of both integer variables on a new line. */
-
-        /* Print the sum of the double variables on a new line. */
-		
-        /* Concatenate and print the String variables on a new line; 
-        	the 's' variable above should be printed first. */
-
-        scan.close();
     }
 }
