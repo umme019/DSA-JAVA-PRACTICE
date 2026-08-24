43ab003e70c088e7393a607b1c0433e3edb7b39a
umme019 <afsanaara88@gmail.com>
2026-08-25 01:07:08 +0530

    Add area and circumference of circle

--- Main.java
+++ Main.java
@@ -4,8 +4,14 @@ public class Main {
 	
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int age = sc.nextInt();
-        System.out.println(age);
+        int radius = sc.nextInt();
+
+        double area= Math.PI*radius*radius;
+        double circumference = 2*Math.PI*radius;
+
+        System.out.println("Area =" + area);
+        System.out.println("Circumference =" + circumference);
+
 
     }
 }
