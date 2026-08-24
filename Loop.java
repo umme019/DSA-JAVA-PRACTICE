d83b2888ec2d6ce488133fb5384c6707bad89b9d
umme019 <afsanaara88@gmail.com>
2026-08-25 01:38:28 +0530

    Add a calculator

--- Loop.java
+++ Loop.java
@@ -2,18 +2,34 @@ import java.util.*;
  
 public class Loop {
     public static void main(String[]args){
-     Scanner sc = new Scanner(System.in);
+     Scanner sc = new Scanner (System.in);
+     System.out.println("Enter 2 numbers a and b");
+     int a = sc.nextInt();
+     int b = sc.nextInt();
 
-        int n = sc.nextInt();
-        int first = 0 ; 
-        int second = 1;
+     System.out.println("Enter the choice = 1,2,3,4,5");
+     int choice = sc.nextInt();
 
-        for (int i =1;i<=n ;i++){
-          System.out.println(first + " ");
-          int next= first + second;
-          first = second ;
-          second = next ;
-        }
+
+     if (choice == 1){
+      System.out.println("Addition =" + (a+b));
+     }else if (choice == 2){
+      System.out.println("Subtraction =" + (a-b));
+     }else if (choice == 3){
+      System.out.println("Multiplication =" + (a*b));
+     }else if (choice == 4){
+
+      if (b == 0){
+        System.out.println("Invalid");
+      }else
+      System.out.println("Division =" + (a/b));
+     }else if (choice == 5){
+       System.out.println("Modulo =" + (a%b));
+     }else{
+      System.out.println("Invalid choice");
+     }
+
+
 
       }
     }
