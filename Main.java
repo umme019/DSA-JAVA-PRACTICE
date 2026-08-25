adf13756715853a925256ba19896f16f0c77e101
umme019 <afsanaara88@gmail.com>
2026-08-25 22:55:31 +0530

    Add a leap year

--- Main.java
+++ Main.java
@@ -4,11 +4,14 @@ public class Main {
 	
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("Enter Celsius");
-        double celsius = sc.nextDouble();
+        System.out.println("Enter year");
+        int year = sc.nextInt();
 
-        double fahrenheit = ((celsius * 9/5) +32 );
-        System.out.println(fahrenheit);
+        if (year % 4 == 0){
+            System.out.println("Leap year");
+        }else{
+            System.out.println("Not a leap year");
+        }
         
          
     }
