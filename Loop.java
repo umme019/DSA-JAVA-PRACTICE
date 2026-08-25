245b6c504697603baf0b92b95578d6fdd67c1d3c
umme019 <afsanaara88@gmail.com>
2026-08-25 22:35:01 +0530

    Add a swap of two numbers

--- Loop.java
+++ Loop.java
@@ -5,12 +5,8 @@ public class Loop {
      Scanner sc = new Scanner (System.in);
      System.out.println("Enter a number");
      int n = sc.nextInt();
-
-     for (int i=0; i<=n;i++){
-      if (i % 2 == 0){
-        System.out.println(i);
-      }
-     }System.out.println();
+      
+     
 
 
       }
--- Main.java
+++ Main.java
@@ -4,18 +4,12 @@ public class Main {
 	
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("Enter table number");
-        int num = sc.nextInt();
+        System.out.println("Enter two number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
 
-       System.out.println(num + "*" + "1" + "=" + num*1);
-       System.out.println(num + "*" + "2" + "=" + num*2);
-       System.out.println(num + "*" + "3" + "=" + num*3);
-       System.out.println(num + "*" + "4" + "=" + num*4);
-       System.out.println(num + "*" + "5" + "=" + num*5);
-       System.out.println(num + "*" + "6" + "=" + num*6);
-       System.out.println(num + "*" + "7" + "=" + num*7);
-       System.out.println(num + "*" + "8" + "=" + num*8);
-       System.out.println(num + "*" + "9" + "=" + num*9);
-       System.out.println(num + "*" + "10" + "=" + num*10);
+        System.out.print(b);
+        System.out.print(a);
     }
 }
