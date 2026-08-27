d38e7ca1e2feec7fed892f3496d261fd7ee58b31
umme019 <afsanaara88@gmail.com>
2026-08-27 21:09:35 +0530

    Add a swap of numbers using XOR

--- Main.java
+++ Main.java
@@ -4,8 +4,19 @@ public class Main {
 	
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("Enter 3 digit number");
-        int n = sc.nextInt();
+        System.out.println("Enter number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        a=a^b;
+        b=a^b;
+        a=a^b;
+
+        System.out.println("a =" + a);
+        System.out.println("b =" + b);
+        
+
+
         
         
         
