d4250b526fb9fb4914ef88c3641aa56e5f19a0f7
umme019 <afsanaara88@gmail.com>
2026-08-27 22:47:47 +0530

    Add a solid rhombus

--- Patterns.java
+++ Patterns.java
@@ -7,13 +7,12 @@ public class Patterns {
          int n = 5;
          
          for (int i=1;i<=n;i++){
-            for (int j=1;j<=i;j++){
-                int sum = i+j;
-                if (sum %2 == 0){//even
-                  System.out.print("1");
-                }else{//odd
-                 System.out.print("0");
-                }
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+
+            for (int j=1;j<=n;j++){
+                System.out.print("*");
             }
             System.out.println();
          }
