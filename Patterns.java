b7b785505e2c4b64baaa2c9ce5c20d61173b2e66
umme019 <afsanaara88@gmail.com>
2026-08-28 06:43:48 +0530

    Add a palindromic number pyramid

--- Patterns.java
+++ Patterns.java
@@ -7,14 +7,23 @@ public class Patterns {
          int n = 5;
          
          for (int i=1;i<=n;i++){
+
+            //space
             for (int j=1;j<=n-i;j++){
-                System.out.print(" ");
+                System.out.print("  ");
             }
 
-            for (int j=1;j<=n;j++){
-                System.out.print("*");
+            //stars
+            for (int j=i;j>=1;j--){
+                System.out.print(j + " ");
             }
-            System.out.println();
+             for (int k=2;k<=i;k++){
+                System.out.print(k + " ");
+            }
+         System.out.println();  
+        }
+        
          }
     }
-    } 
+    
+
