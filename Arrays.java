d3b5b56c3d000f3b014ad35b8fae6bb79ad2d9d8
umme019 <afsanaara88@gmail.com>
2026-09-03 23:41:51 +0530

    Add a Calculator

--- Arrays.java
+++ Arrays.java
@@ -4,17 +4,20 @@ public class Arrays{
     public static void main (String args[]){
         Scanner sc = new Scanner (System.in);
 
-     int size = sc.nextInt();
-     int number []= new int [size];
+     int n = sc.nextInt();
+     int m = sc.nextInt();
+     int [][]matrix= new int [n][m];
 
      //input
-     for (int i=0;i<size ;i++){
-     number [i]=sc.nextInt();
+     //n
+     for (int i=0;i<n ;i++){
+     //m
+     for (int j=0;j<m;j++){
+        matrix [i][j]=sc.nextInt();
+     }
      }
      //output
-     int evenCount=0;
-     int oddCount=0;
-     for (int i=0;i<size;i++){
+     for (int j=0;j<m;j++){
      if (number[i]%2==0){
         evenCount=evenCount+1;
 
--- Loop.java
+++ Loop.java
@@ -3,25 +3,30 @@ import java.util.*;
 public class Loop {
     public static void main(String[]args){
      Scanner sc = new Scanner (System.in);
-     System.out.println("Enter a number");
-     int n = sc.nextInt();
-     int original = n;
-      
-     int reversed = 0;
-     while(n>0){
-       reversed= reversed*10 +n%10;
-       n= n/10;
-     }
+     System.out.println("Enter two number");
+     int a = sc.nextInt();
+     int b = sc.nextInt();
+     int choice = sc.nextInt();
      
-     if (original == reversed){
-      System.out.println("Palindrome");
-     }else{
-      System.out.println("Not Palindrome");
-     }
+      switch (choice){
+        case 1: System.out.println("Addition = " + (a+b));
+        break;
+        case 2: System.out.println("Subtraction = " + (a-b));
+        break;
+        case 3: System.out.println("Multiplication = " + (a*b));
+        break;
+        case 4: if (b==0){
+          System.out.println("Invalid");
+        }else{
+        System.out.println("Division = " + (a/b));
+        }
+        break;
+        case 5: System.out.println("Modulus = " + (a%b));
+        break;
+        default: System.out.println("Invalid");
+      }
     
     
-
-
       }
     }
 
